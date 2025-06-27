a943de07bcfae0f1b56d018f189446d9e944d21b
Stanimir Kostov <stanimirkostov24@gmail.com>
2025-06-27 22:28:58 +0300

    Add function for opening file explorer

--- app.py
+++ app.py
@@ -1,10 +1,34 @@
 from tkinter import *
+from tkinter import ttk,filedialog
+from tkinter.ttk import *
 
 root = Tk()
 root.title('JPEG to PDF Converter')
 root.geometry('600x600')
 
-main_frame = ttk.Frame(root)
+top_frame = Frame(root)
+top_frame.pack()
+
+def browse_files():
+    filename = filedialog.askopenfilename(title="Select image",
+                                          filetypes=[("image files","*.jpeg *.png *.jpg")])
+    label_file_explorer.configure( text="Chosen image: \n" + filename)
+
+files_btn = ttk.Button(top_frame, text="Choose image",command=browse_files)
+files_btn.pack(anchor="center")
+
+# files_box = Text(top_frame,height=20,width=80,bg='light yellow')
+# files_box.pack(anchor='center')
+
+bot_frame = Frame(root)
+bot_frame.pack(side="bottom")
+
+
+
+
+label_file_explorer  = ttk.Label(top_frame,text='')
+label_file_explorer.pack()
+
 
 root.mainloop()
 
