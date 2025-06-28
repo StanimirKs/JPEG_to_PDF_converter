f793b15bac69b76b304b6fa3cfef41494fe4b2e0
Stanimir Kostov <stanimirkostov24@gmail.com>
2025-06-28 21:22:35 +0300

    Cleaned up interface and now shows only filename instead of path

--- app.py
+++ app.py
@@ -1,33 +1,52 @@
 from tkinter import *
 from tkinter import ttk,filedialog
 from tkinter.ttk import *
+import os
 
 root = Tk()
 root.title('JPEG to PDF Converter')
-root.geometry('600x600')
+root.geometry('500x400')
+
+root.grid_rowconfigure(0,weight=2)
+root.grid_rowconfigure(1,weight=1)
+root.grid_columnconfigure(1,weight=3)
 
 top_frame = Frame(root)
-top_frame.pack()
+top_frame.grid(row=0,column=1)
+
+hint = ttk.Label(top_frame,text="Select Image")
+hint.pack()
 
 def browse_files():
-    filename = filedialog.askopenfilename(title="Select image",
-                                          filetypes=[("image files","*.jpeg *.png *.jpg")])
-    label_file_explorer.configure( text="Chosen image: \n" + filename)
+    fulll_path = filedialog.askopenfilename(title="Select image", filetypes=[("image files","*.jpeg *.png *.jpg")])
+    filename = os.path.basename(fulll_path)
+    label_file_explorer.configure(text=f"{filename}")
+    hint.configure(text='Selected Image: \n')
 
-files_btn = ttk.Button(top_frame, text="Choose image",command=browse_files)
-files_btn.pack(anchor="center")
+
 
 # files_box = Text(top_frame,height=20,width=80,bg='light yellow')
 # files_box.pack(anchor='center')
 
-bot_frame = Frame(root)
-bot_frame.pack(side="bottom")
-
-
-
 
 label_file_explorer  = ttk.Label(top_frame,text='')
 label_file_explorer.pack()
+
+
+
+
+bot_frame = Frame(root)
+bot_frame.grid(row=1,column=1)
+
+files_btn = ttk.Button(bot_frame, text="Choose image",command=browse_files)
+files_btn.pack()
+
+convert_btn = ttk.Button(bot_frame,text="Convert Image to PDF")
+convert_btn.pack()
+
+
+
+
 
 
 root.mainloop()
